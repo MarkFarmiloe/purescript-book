a540da206f882864fc9d60439a538b136a747a12
Mark Farmiloe <mark@ccsdev.net>
2023-07-03 10:25:01 +0100

    Chapters 2 and 3 done

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,32 @@
-module Test.MySolutions where
+module Test.MySolutions
+  ( answer
+  , circleArea
+  , diagonal
+  , leftoverCents
+  )
+  where
 
 import Prelude
+
+import Data.Foldable (sum)
+import Data.Int (rem)
+import Data.List (List, range, filter)
+import Data.Number (pi, sqrt)
+
+ns:: Int -> List Int
+ns n = range 0 (n - 1)
+
+factorOf3or5:: List Int -> List Int
+factorOf3or5 = filter (\n -> mod n 3 == 0 || mod n 5 == 0)
+
+answer:: Int -> Int
+answer n = sum (factorOf3or5 (ns n))
+
+diagonal:: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea:: Number -> Number
+circleArea r = pi * r * r
+
+leftoverCents:: Int -> Int
+leftoverCents n = rem n 100
--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,4 +2,23 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.AddressBook (Entry, AddressBook)
+import Data.List (filter, head, nubByEq, null)
+import Data.Maybe (Maybe)
+
 -- Note to reader: Add your solutions to this file
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter ( eq street <<< _.address.street)
+
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook fn ln = not null <<< filter filterEntry
+    where
+      filterEntry :: Entry -> Boolean
+      filterEntry e = e.firstName == fn && e.lastName == ln
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubByEq compareEntries
+    where
+      compareEntries :: Entry -> Entry -> Boolean
+      compareEntries a b = a.firstName == b.firstName && a.lastName == b.lastName
